1aed82843d55ac82bfe26ac3ad2718c3847f326a
Haardik <hhaardik@uwaterloo.ca>
2021-03-29 23:37:34 -0400

    update banner css

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -3,17 +3,17 @@ import logo from "../../assets/flexfintx-green-logo.png";
 
 function Banner() {
   return (
-    <section className="section has-background-primary">
+    <section className="section has-background-dark">
       <div className="container is-fullhd">
-        <div className="is-flex is-flex-direction-column content has-text-centered is-align-items-center">
+        <div className="is-flex is-flex-direction-column content has-text-white has-text-centered is-align-items-center">
           <figure className="image is-128x128">
             <a href="https://flexfintx.com">
               <img src={logo} alt="" />
             </a>
           </figure>
           <br />
-          <h1>Welcome to FlexLand</h1>
-          <h4>The future of Africa!</h4>
+          <h1 className="has-text-light">Welcome to FlexLand</h1>
+          <h4 className="has-text-light">The future of Africa!</h4>
           <br /><br />
         </div>
       </div>
